7ceecea8f4b0c12ff272e4061f8d23fecaf84ef5
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 11:53:18 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    * use the centralized publish workflow for compatibility with
      hatchling/pyproject.toml
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2020 CERN.
-# SPDX-License-Identifier: MIT
-
-"""Python module to ease the creation and management of external services."""
-
-from setuptools import setup
-
-setup()
